d53fd9bcf088f01292eb5e8584f6f3e9c141987d
马军 <majun@xiwicloud.com>
2025-03-18 20:49:33 +0800

    fix: 部署脚本加兼容,防止备份文件夹没有

--- lib/remote-deploy.sh
+++ lib/remote-deploy.sh
@@ -14,11 +14,14 @@ if [ ! -e "$LOCAL_TAR_FILE" ]; then
   exit 1
 fi
 
+# 确保 REMOTE_BACKDIR 目录存在
+mkdir -p "$REMOTE_BACKDIR" || { echo "创建备份目录失败"; exit 1; }
+
 if [ -e "$REMOTE_DISTNAME" ]; then
   mv "$REMOTE_DISTNAME" "${REMOTE_DISTNAME}_${CURRENT_TIMESTAMP}" || { echo "重命名失败"; exit 1; }
   echo "旧目录已重命名为 ${REMOTE_DISTNAME}_${CURRENT_TIMESTAMP}"
-  mv "${REMOTE_DISTNAME}_${CURRENT_TIMESTAMP}" "${REMOTE_BACKDIR}${REMOTE_DISTNAME}_${CURRENT_TIMESTAMP}" || { echo "移动到备份目录失败"; exit 1; }
-  echo "旧目录已移动到备份目录 ${REMOTE_BACKDIR}${REMOTE_DISTNAME}_${CURRENT_TIMESTAMP}"
+  mv "${REMOTE_DISTNAME}_${CURRENT_TIMESTAMP}" "${REMOTE_BACKDIR}/${REMOTE_DISTNAME}_${CURRENT_TIMESTAMP}" || { echo "移动到备份目录失败"; exit 1; }
+  echo "旧目录已移动到备份目录 ${REMOTE_BACKDIR}/${REMOTE_DISTNAME}_${CURRENT_TIMESTAMP}"
 fi
 
 tar -xvf "$LOCAL_TAR_FILE" || { echo "解压失败"; exit 1; }
